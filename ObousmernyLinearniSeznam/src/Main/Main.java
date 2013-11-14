63acbbc354a3e0a516eaaa85a745898137193d07
johnny1212 <johnny1212@volny.cz>
2013-11-14 20:54:36 +0100

    tvorba - Lineární seznam

--- ObousmernyLinearniSeznam/src/Main/Main.java
+++ ObousmernyLinearniSeznam/src/Main/Main.java
@@ -13,7 +13,7 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        System.out.println("LINEÁRNÍ SEZNAM");
+        System.out.println("LINEÁRNÍ SEZNAM22");
         LinearniSeznam sez = new LinearniSeznam();
         sez.addNodeFirst("C");
         sez.addNodeFirst("B");
