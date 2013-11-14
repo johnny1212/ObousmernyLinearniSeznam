fb0dc4af183618c030ef959d297ad99af7fe09df
johnny1212 <johnny1212@volny.cz>
2013-11-14 20:08:46 +0100

    Finala

--- ObousmernyLinearniSeznam/src/LinearniSeznam/LinearniSeznam.java
+++ ObousmernyLinearniSeznam/src/LinearniSeznam/LinearniSeznam.java
@@ -23,6 +23,32 @@ public class LinearniSeznam {
         return first.getData();
     }
 
+    public Object getLast() {
+        if (first == null) {
+            return null;
+        }
+        actual = first;
+        while (actual != null) {
+            if (actual.getAfter() == null) {
+                return actual.getData();
+            }
+            actual = actual.getAfter();
+        }
+        actual = first;
+        return first.getData();
+    }
+
+    public Object getAfterActual() {
+        if (first == null) {
+            return null;
+        }
+        actual = actual.getAfter();
+        if (actual == null) {
+            return null;
+        }
+        return actual.getData();
+    }
+
     public void addNodeFirst(Object data) {
         Node node = new Node(data);
         if (first == null) {
@@ -36,6 +62,40 @@ public class LinearniSeznam {
         actual = node;
     }
 
+    public void addNodeAfterActual(Object data) {
+        if (first == null) {
+            addNodeFirst(data);
+            return;
+        }
+        Node node = new Node(data);
+        if (actual.getAfter() != null) {
+            actual.getAfter().setBefore(node);
+            node.setAfter(actual.getAfter());
+        }
+        node.setBefore(actual);
+        actual.setAfter(node);
+        actual = node;
+    }
+
+    public void addNodeLast(Object data) {
+        if (first == null) {
+            addNodeFirst(data);
+            return;
+        }
+        getLast();
+        Node node = new Node(data);
+        node.setBefore(actual);
+        actual.setAfter(node);
+        actual = node;
+    }
+
+    /*public void vypis() {
+     actual = first;
+     while (actual != null) {
+     System.out.println("-" + actual.getData());
+     actual = actual.getAfter();
+     }
+     }*/
     public Iterator getIterator() {
         return new Iter();
     }
