dd6cde9c3a31f9c64642c255113e32737db972e8
johnny1212 <johnny1212@volny.cz>
2013-11-14 19:55:03 +0100

    Finala

---
+++ ObousmernyLinearniSeznam/src/obousmernylinearniseznam/ObousmernyLinearniSeznam.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package obousmernylinearniseznam;
+
+/**
+ *
+ * @author Johnny
+ */
+public class ObousmernyLinearniSeznam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("AAAAA");
+        System.out.println("AAAAAB");
+    }
+}
